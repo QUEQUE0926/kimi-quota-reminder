7f6dfb1bedc7bebaf78303392aa82a8ead8cb511
QUEQUE0926 <49783105+QUEQUE0926@users.noreply.github.com>
2026-09-06 14:28:54 +0800

    add WxPusher SPT channel (plan §6 fourth push channel)

--- scripts/manual.mjs
+++ scripts/manual.mjs
@@ -50,6 +50,7 @@ switch (action) {
       ['企业微信群机器人', !!process.env.WECOM_WEBHOOK],
       ['ntfy', !!process.env.NTFY_TOPIC],
       ['Bark', !!process.env.BARK_KEY],
+      ['WxPusher', !!process.env.WXPUSHER_SPT],
     ];
     const active = channels.filter(([, on]) => on).map(([n]) => n);
     const inactive = channels.filter(([, on]) => !on).map(([n]) => n);
